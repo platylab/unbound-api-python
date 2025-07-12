f89578e758aab5a9923c884362d2093a2e5c87e1
Arthur Réauté <arthur@platy.bzh>
2025-07-12 22:01:11 +0200

    refactor(UnboundConfig): add methods to generate and validate file

--- src/unboundapi/config/UnboundConfig.py
+++ src/unboundapi/config/UnboundConfig.py
@@ -1,4 +1,5 @@
 from unboundapi.config.ConfigEntry import ConfigEntry
+import subprocess
 
 
 class UnboundConfigError(Exception):
@@ -21,23 +22,6 @@ class DuplicateIDError(UnboundConfigError):
 
 class UnboundConfig:
     __supported_attributes = {
-        "server": {
-            "module-config": dict(),
-            "interface": dict(),
-            "port": dict(),
-            "prefer-ip4": dict(),
-            "do-ip4": dict(),
-            "do-ip6": dict(),
-            "do-udp": dict(),
-            "do-tcp": dict(),
-            "access-control": dict(),
-            "interface-action": dict(),
-            "serve-expired": dict(),
-            "serve-expired-ttl": dict(),
-            "serve-expired-client-timeout": dict(),
-            "local-zone": dict(),
-            "local-data": dict(),
-        },
         "remote-control": {
             "control-enable": dict(),
             "control-interface": dict(),
@@ -58,14 +42,51 @@ class UnboundConfig:
             "dnscrypt-enable": dict(),
             "dnscrypt-port": dict(),
         },
+        "server": {
+            "module-config": dict(),
+            "interface": dict(),
+            "port": dict(),
+            "prefer-ip4": dict(),
+            "do-ip4": dict(),
+            "do-ip6": dict(),
+            "do-udp": dict(),
+            "do-tcp": dict(),
+            "access-control": dict(),
+            "interface-action": dict(),
+            "serve-expired": dict(),
+            "serve-expired-ttl": dict(),
+            "serve-expired-client-timeout": dict(),
+            "local-zone": dict(),
+            "local-data": dict(),
+        },
     }
 
     def __init__(self, config_file: str = "/etc/unboud/unboud.conf"):
-        self.server = UnboundConfig.__supported_attributes["server"]
         self.remote_control = UnboundConfig.__supported_attributes["remote-control"]
         self.forward_zone = UnboundConfig.__supported_attributes["forward-zone"]
         self.auth_zone = UnboundConfig.__supported_attributes["auth-zone"]
         self.dnscrypt = UnboundConfig.__supported_attributes["dnscrypt"]
+        self.server = UnboundConfig.__supported_attributes["server"]
+        self.__load_config(config_file)
+
+    def __str__(self) -> str:
+        return str(self.to_dict())
+
+    def to_dict(self):
+        return {
+            "remote-control": self.remote_control,
+            "forward-zone": self.forward_zone,
+            "auth-zone": self.auth_zone,
+            "dnscrypt": self.dnscrypt,
+            "server": self.server,
+        }
+
+    def __load_config(self, config_file: str = "/etc/unboud/unboud.conf"):
+        self.remote_control = UnboundConfig.__supported_attributes["remote-control"]
+        self.forward_zone = UnboundConfig.__supported_attributes["forward-zone"]
+        self.auth_zone = UnboundConfig.__supported_attributes["auth-zone"]
+        self.dnscrypt = UnboundConfig.__supported_attributes["dnscrypt"]
+        self.server = UnboundConfig.__supported_attributes["server"]
 
         with open(config_file, "r") as file:
             current_clause = ""
@@ -93,14 +114,36 @@ class UnboundConfig:
                     else:
                         raise DuplicateIDError(entry.attribute, entry.line_nb, entry.id)
 
-    def to_dict(self):
-        return {
-            "server": self.server,
-            "remote-control": self.remote_control,
-            "forward-zone": self.forward_zone,
-            "auth-zone": self.auth_zone,
-            "dnscrypt": self.dnscrypt,
-        }
+    def clear(self):
+        """Clear the current configuration data."""
+        for attribute in UnboundConfig.__supported_attributes.keys():
+            getattr(self, attribute.replace("-", "_")).clear()
 
-    def __str__(self):
-        return str(self.to_dict())
+    def reload_config(self, config_file: str = "/etc/unboud/unboud.conf"):
+        self.clear()
+        self.__load_config(config_file)
+
+    def make(self, target_file: str) -> str:
+        """Create the config file from the stored config. The file must not be existant"""
+        lines_to_write = list()
+        for clause in UnboundConfig.__supported_attributes.keys():
+            clause_lines = list()
+            clause_lines.append(f"{clause}:")
+            for attribute, data in UnboundConfig.__supported_attributes[clause].items():
+                for value_id, value in data.items():
+                    clause_lines.append(f"  {attribute}: {value} #{value_id}")
+            clause_lines.append("")
+            if len(clause_lines) > 2:
+                lines_to_write += clause_lines
+        with open(target_file, "w") as file:
+            for line in lines_to_write:
+                file.write(line + "\n")
+        return target_file
+
+    def validate(self, target_file: str) -> subprocess.CompletedProcess:
+        """Verify that the created file is valid with unboud-checkconf"""
+        return subprocess.run(
+            ["unbound-checkconf", target_file],
+            capture_output=True,
+            text=True,
+        )

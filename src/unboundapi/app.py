06569bc0b1b8bb045bfff6c2538704cfc3af2a31
Arthur Morel-Réauté <arthur@platy.bzh>
2026-03-02 23:20:01 +0100

    feat(api): :sparkles: add JWT auth with username and password hash from env
    password hash can be generated by `ansible localhost -m debug -a "msg='{{ 'password' | password_hash('sha512') }}'" --connection=local`
    
    login is now required

--- src/unboundapi/app.py
+++ src/unboundapi/app.py
@@ -1,5 +1,13 @@
+import os
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request, jsonify
+from datetime import datetime, timedelta
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    get_jwt,
+)
 from unboundapi.main import main as unbound_config
 from unboundapi.config.UnboundConfig import (
     DuplicateIDError,
@@ -7,18 +15,59 @@ from unboundapi.config.UnboundConfig import (
     UnsupportedClauseError,
     UnsupportedAttributeError,
 )
-
-load_dotenv(find_dotenv())
-load_dotenv(find_dotenv(".env.local"))
+from passlib.hash import sha512_crypt
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["JWT_SECRET_KEY"] = os.environ["UNBOUNDAPI_JWT_SECRET_KEY"]
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(
+    seconds=int(
+        os.environ.get(
+            "JWT_ACCESS_TOKEN_EXPIRES",
+            "3600",
+        ),
+    ),
+)
+jwt = JWTManager(app)
+
+admin_username = os.environ.get("UNBOUNDAPI_ADMIN_USERNAME", "admin")
+admin_password_hash = os.environ["UNBOUNDAPI_ADMIN_PASSWORD_HASH"]
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username")
+    password = request.json.get("password")
+
+    # Input validation
+    if not username or not password:
+        return jsonify({"msg": "Missing username or password"}), 400
+
+    # Authentication
+    if username == admin_username and sha512_crypt.verify(
+        password,
+        admin_password_hash,
+    ):
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "Invalid credentials"}), 401
+
+
+@app.route("/whoami", methods=["GET"])
+@jwt_required()
+def whoami():
+    token_data = get_jwt()
+    username = token_data["sub"]
+    expires_in = int(token_data["exp"] - datetime.now().timestamp())
+    return jsonify(username=username, token_expires_in=expires_in), 200
 
 
 @app.route(
     "/config/<clause>/<attribute>/<value_id>",
     methods=["GET", "POST", "PUT", "DELETE"],
 )
+@jwt_required()
 def configure(clause, attribute, value_id):
     if value_id == "*":
         value_id = ""
@@ -120,4 +169,6 @@ def configure(clause, attribute, value_id):
 
 
 if __name__ == "__main__":
+    load_dotenv(find_dotenv())
+    load_dotenv(find_dotenv(".env.local"))
     app.run(host="127.0.0.1", port=8080, debug=True)
--- src/unboundapi/server.py
+++ src/unboundapi/server.py
@@ -1,9 +1,12 @@
+from dotenv import load_dotenv, find_dotenv
 import os
 import sys
 from subprocess import run
 
 
 def start():
+    load_dotenv(find_dotenv())
+    load_dotenv(find_dotenv(".env.local"))
     config_path = os.path.join(os.path.dirname(__file__), "gunicorn_config.py")
     run(
         [

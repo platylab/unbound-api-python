21a2532708f50e12aecf6309d5d0e65a05b4a4fe
Arthur Morel-Réauté <arthur@platy.bzh>
2026-03-03 00:46:43 +0100

    fix: :loud_sound: use X-Forwarded-For header logging format

--- src/unboundapi/gunicorn_config.py
+++ src/unboundapi/gunicorn_config.py
@@ -9,6 +9,9 @@ bind = os.environ.get("UNBOUNDAPI_BIND", "127.0.0.1:8080")
 forwarded_allow_ips = "*"
 secure_scheme_headers = {"X-Forwarded-Proto": "https"}
 
-# logging
+# Logging
 accesslog = os.environ.get("UNBOUNDAPI_ACCESSLOG", "-")
 errorlog = os.environ.get("UNBOUNDAPI_ERRORLOG", "-")
+
+# Custom access log format
+access_log_format = '%({x-forwarded-for}i)s %(l.md)s %(u.md)s %(t.md)s "%(r.md)s" %(s.md)s %(b.md)s "%(f.md)s" "%(a.md)s"'
